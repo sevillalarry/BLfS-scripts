4379306b40436d577db65c197ddab8f131801edf
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-04-26 16:28:45 +0800

    Chapter 24, last

--- 11.3.b/b.24.27.xclock-1.1.1.sh
+++ 11.3.b/b.24.27.xclock-1.1.1.sh
@@ -1,13 +1,11 @@
-# b.24.23.Xorg-Server-21.1.7.sh
+# b.24.27.xclock-1.1.1.sh
 #
 
-export PKG="Xorg-Server-21.1.7"
-export PKGLOG_DIR=$LFSLOG/24.23
+export PKG="xclock-1.1.1"
+export PKGLOG_DIR=$LFSLOG/24.27
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
-#export PKGLOG_CHECK=$PKGLOG_DIR/check.log
-#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
@@ -22,41 +20,26 @@ tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
 cd $PKG
 
 
-mkdir build
-cd    build
-
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
-meson   --prefix=$XORG_PREFIX \
-        --localstatedir=/var  \
-        -Dsuid_wrapper=true   \
-         -Dxkb_output_dir=/var/lib/xkb
-        > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+./configure $XORG_CONFIG    \
+     > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
 echo "3. Make Build ..."
 echo "3. Make Build ..." >> $LFSLOG_PROCESS
 echo "3. Make Build ..." >> $PKGLOG_ERROR
-ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
 echo "4. Make Install ..."
 echo "4. Make Install ..." >> $LFSLOG_PROCESS
 echo "4. Make Install ..." >> $PKGLOG_ERROR
-ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 
-mkdir -pv /etc/X11/xorg.conf.d
-cat >> /etc/sysconfig/createfiles <<
-/tmp/.ICE-unix dir 1777 root root
-/tmp/.X11-unix dir 1777 root root
-
-
-cd ..
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
-#unset PKGLOG_OTHERS
-#unset PKGLOG_CHECK
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
