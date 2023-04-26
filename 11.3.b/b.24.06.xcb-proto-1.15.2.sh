1dbe9016aecdf51f94c30eb2d9c5f699b3f3b7b1
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-04-26 11:23:25 +0800

    Chapter 24, initial

---
+++ 11.3.b/b.24.06.xcb-proto-1.15.2.sh
@@ -0,0 +1,54 @@
+# b.24.06.xcb-proto-1.15.2.sh
+#
+
+export PKG="xcb-proto-1.15.2"
+export PKGLOG_DIR=$LFSLOG/24.06
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+#export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. Configure ..."
+echo "2. Configure ..." >> $LFSLOG_PROCESS
+echo "2. Configure ..." >> $PKGLOG_ERROR
+PYTHON=python3 \
+./configure $XORG_CONFIG    \
+     > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Make Check ..."
+echo "3. Make Check ..." >> $LFSLOG_PROCESS
+echo "3. Make Check ..." >> $PKGLOG_ERROR
+make check > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "4. Make Install ..."
+echo "4. Make Install ..." >> $LFSLOG_PROCESS
+echo "4. Make Install ..." >> $PKGLOG_ERROR
+make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+rm -f $XORG_PREFIX/lib/pkgconfig/xcb-proto.pc
+
+
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+#unset PKGLOG_OTHERS
+unset PKGLOG_CHECK
+unset PKGLOG_INSTALL
+#PKGLOG_BUILD
+unset PKGLOG_CONFIG
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
---
+++ 11.3.b/b.24.16.Mesa-22.3.5.sh
@@ -0,0 +1,62 @@
+# b.24.16.Mesa-22.3.5.sh
+#
+
+export PKG="Mesa-22.3.5"
+export PKGLOG_DIR=$LFSLOG/24.16
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+#export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+mkdir build
+cd    build
+
+echo "2. Configure ..."
+echo "2. Configure ..." >> $LFSLOG_PROCESS
+echo "2. Configure ..." >> $PKGLOG_ERROR
+meson   --prefix=$XORG_PREFIX   \
+        --buildtype=release     \
+        -Dplatforms=x11,wayland \
+        -Dgallium-drivers=auto  \
+        -Dvulkan-drivers=""     \
+        -Dvalgrind=disabled     \
+        -Dlibunwind=disabled    \
+        > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Make Build ..."
+echo "3. Make Build ..." >> $LFSLOG_PROCESS
+echo "3. Make Build ..." >> $PKGLOG_ERROR
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Make Install ..."
+echo "4. Make Install ..." >> $LFSLOG_PROCESS
+echo "4. Make Install ..." >> $PKGLOG_ERROR
+ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+install -dm755 /usr/share/doc/mesa-22.3.5
+cp -rf ../docs/* /usr/share/doc/mesa-22.3.5
+
+
+cd ..
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+#unset PKGLOG_OTHERS
+#unset PKGLOG_CHECK
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
