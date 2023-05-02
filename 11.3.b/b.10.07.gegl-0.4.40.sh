1ad0b90a3c8e0174881ecfc4523e337451013d5d
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-05-02 16:07:04 +0800

    Chapter 10, initial

---
+++ 11.3.b/b.10.07.gegl-0.4.40.sh
@@ -0,0 +1,60 @@
+#b.10.07.gegl-0.4.40.sh
+
+export PKG="gegl-0.4.40"
+export PKGLOG_DIR=$LFSLOG/10.07
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+sed -e '/shot_select/s/params/raw&/'    \
+    -i operations/external/raw-load.c   \
+
+
+mkdir build
+cd    build
+
+echo "2. Configure ..."
+echo "2. Configure ..." >> $LFSLOG_PROCESS
+echo "2. Configure ..." >> $PKGLOG_ERROR
+meson --prefix=/usr           \
+      --buildtype=release     \
+          > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Make Build ..."
+echo "3. Make Build ..." >> $LFSLOG_PROCESS
+echo "3. Make Build ..." >> $PKGLOG_ERROR
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Make Check ..."
+echo "4. Make Check ..." >> $LFSLOG_PROCESS
+echo "4. Make Check ..." >> $PKGLOG_ERROR
+ninja test > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Make Install ..."
+echo "5. Make Install ..." >> $LFSLOG_PROCESS
+echo "5. Make Install ..." >> $PKGLOG_ERROR
+ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+
+cd ..
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
