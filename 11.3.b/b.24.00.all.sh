467c7b12ab82242099643ade42149eeedc2a6e6d
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-05 09:21:33 +0800

    2023/Aug/05 09:20

---
+++ 11.3.b/b.24.00.all.sh
@@ -0,0 +1,12 @@
+# b.24.00.all.sh
+#
+# Graphical Environments
+#
+
+bash b.24.00.Part1.02-07.sh
+bash b.24.00.Part2.08.sh
+bash b.24.00.Part3.09-17.sh
+bash b.24.00.Part4.18.sh
+bash b.24.00.Part5.19.sh
+bash b.24.00.Part6.20.sh
+bash b.24.00.Part7.21-28.sh
