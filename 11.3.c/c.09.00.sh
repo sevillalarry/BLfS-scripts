6f716b57086d67c9c2af2c7c002b8e1d9f58cf5a
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-10 14:26:02 +0800

    2023/Aug/10 14:26

--- 11.3.c/c.09.00.sh
+++ 11.3.c/c.09.00.sh
@@ -37,6 +37,8 @@ echo "."
 echo "."
 
 ###
+#   compile ahead
+###
 
 echo "11.04.desktop-file-utils-0.26"
 echo "11.04.desktop-file-utils-0.26" >> $LFSLOG_PROCESS
@@ -66,7 +68,6 @@ echo "."
 
 ###
 
-# compile ahead. needed by some GLib tests
 echo "12.10.dbus-1.14.8"
 echo "12.10.dbus-1.14.8" >> $LFSLOG_PROCESS
 
@@ -84,7 +85,21 @@ echo "."
 echo "09.13.GLib-2.74.5.test"
 echo "09.13.GLib-2.74.5.test" >> $LFSLOG_PROCESS
 
-time { bash c.09.13.GLib-2.74.5.sh.test ; }
+time { bash c.09.13.GLib-2.74.5.test.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "09.40.libgudev-237"
+echo "09.40.libgudev-237" >> $LFSLOG_PROCESS
+
+time { bash c.09.40.libgudev-237.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
