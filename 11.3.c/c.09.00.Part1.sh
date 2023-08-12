febfd4c75d4eff69f3ba56957402b6e1c6121ef6
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-12 12:46:43 +0800

    2023/Aug/12 12:45

--- 11.3.c/c.09.00.Part1.sh
+++ 11.3.c/c.09.00.Part1.sh
@@ -12,6 +12,21 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
 #####
 
+# compile ahead.  needed by GLib
+echo "09.68.libxslt-1.1.37"
+echo "09.68.libxslt-1.1.37" >> $LFSLOG_PROCESS
+
+time { bash c.09.68.libxslt-1.1.37.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
 echo "09.13.GLib-2.74.5"
 echo "09.13.GLib-2.74.5" >> $LFSLOG_PROCESS
 
