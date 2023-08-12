deb97354c6272ac6ea06e58fc6739ed1cfb49277
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-12 20:25:29 +0800

    2023/Aug/12 20:25

--- 11.3.c/c.09.00.sh
+++ 11.3.c/c.09.00.sh
@@ -1,8 +1,6 @@
-# c.09.00.Part1.sh
+# c.09.00.sh
 #
 # General Libraries
-#
-# Before GLib tests
 #
 
 export LFSLOG_PROCESS=$LFSLOG/process.log
@@ -116,68 +114,10 @@ echo "."
 
 ###
 
-echo "09.16.gobject-introspection-1.74.0"
-echo "09.16.gobject-introspection-1.74.0" >> $LFSLOG_PROCESS
-
-time { bash c.09.16.gobject-introspection-1.74.0.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
-###
-#   compile ahead
-###
-
-echo "11.04.desktop-file-utils-0.26"
-echo "11.04.desktop-file-utils-0.26" >> $LFSLOG_PROCESS
-
-time { bash c.11.04.desktop-file-utils-0.26.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
-###
-
-echo "11.17.shared-mime-info-2.2"
-echo "11.17.shared-mime-info-2.2" >> $LFSLOG_PROCESS
-
-time { bash c.11.17.shared-mime-info-2.2.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
-###
-
 echo "12.10.dbus-1.14.8"
 echo "12.10.dbus-1.14.8" >> $LFSLOG_PROCESS
 
 time { bash c.12.10.dbus-1.14.8.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
-###
-
-echo "09.40.libgudev-237"
-echo "09.40.libgudev-237" >> $LFSLOG_PROCESS
-
-time { bash c.09.40.libgudev-237.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
