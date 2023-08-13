a0e6c06d7a2c95cd0ff3bfaef07221c967fd2698
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-08-13 21:34:54 +0800

    2023/Aug/13 21:35 bug fix

--- 11.3.c/c.09.07.dbus-glib-0.112.sh
+++ 11.3.c/c.09.07.dbus-glib-0.112.sh
@@ -34,7 +34,7 @@ cd $PKG
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
-../configure --prefix=/usr     \
+./configure --prefix=/usr     \
             --sysconfdir=/etc  \
             --disable-static   \
           > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
