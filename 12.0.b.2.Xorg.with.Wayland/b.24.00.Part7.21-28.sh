2c26c2d551f87fbafaf2bc0cf3df28f98fc54a64
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-09-14 16:17:41 +0800

    update Xorg with Others

---
+++ 12.0.b.2.Xorg.with.Wayland/b.24.00.Part7.21-28.sh
@@ -0,0 +1,180 @@
+# b.24.00.Part7.21-28.sh
+#
+# Graphical Environments
+#
+#   24.21
+#           skip 24.22 Xwayland
+#   24.23
+#   24.24.1.1
+#   24.24.1.2
+#   24.24.2.3
+#
+#   24.25
+#   to
+#   24.28
+#
+
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+#####
+
+echo "24.21.XKeyboardConfig-2.39"
+echo "24.21.XKeyboardConfig-2.39" >> $LFSLOG_PROCESS
+
+time { bash b.24.21.XKeyboardConfig-2.39.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+#       libepoxy is recommended but needed by Xorg-Server
+###
+
+echo "25.38.libepoxy-1.5.10"
+echo "25.38.libepoxy-1.5.10" >> $LFSLOG_PROCESS
+
+time { bash b.25.38.libepoxy-1.5.10.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.22.Xwayland-23.2.0"
+echo "24.22.Xwayland-23.2.0" >> $LFSLOG_PROCESS
+
+time { bash b.24.22.Xwayland-23.2.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.23.Xorg-Server-21.1.8"
+echo "24.23.Xorg-Server-21.1.8" >> $LFSLOG_PROCESS
+
+time { bash b.24.23.Xorg-Server-21.1.8.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.24.1.1.libevdev-1.13.1"
+echo "24.24.1.1.libevdev-1.13.1" >> $LFSLOG_PROCESS
+
+time { bash b.24.24.1.1.libevdev-1.13.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.24.1.2.Xorg.Evdev.Driver-2.10.6"
+echo "24.24.1.2.Xorg.Evdev.Driver-2.10.6" >> $LFSLOG_PROCESS
+
+time { bash b.24.24.1.2.Xorg.Evdev.Driver-2.10.6.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.25.twm-1.0.12"
+echo "24.25.twm-1.0.12" >> $LFSLOG_PROCESS
+
+time { bash b.24.25.twm-1.0.12.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.26.xterm-384"
+echo "24.26.xterm-384" >> $LFSLOG_PROCESS
+
+time { bash b.24.26.xterm-384.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.27.xclock-1.1.1"
+echo "24.27.xclock-1.1.1" >> $LFSLOG_PROCESS
+
+time { bash b.24.27.xclock-1.1.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.28.xinit-1.4.2"
+echo "24.28.xinit-1.4.2" >> $LFSLOG_PROCESS
+
+time { bash b.24.28.xinit-1.4.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "24.31.04.dejavu-fonts-ttf-2.37"
+echo "24.31.04.dejavu-fonts-ttf-2.37" >> $LFSLOG_PROCESS
+
+time { bash b.24.31.04.dejavu-fonts-ttf-2.37.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+#####
+
+unset LFSLOG_PROCESS
---
+++ 12.0.b.3.Xorg.with.Others/b.24.24.1.3.libinput-1.23.0.sh
@@ -0,0 +1,73 @@
+# b.24.24.1.3.libinput-1.23.0.sh
+#
+# Dependencies Required:
+#
+#    24.24.1.1 libevdev 1.13.1
+#    09.73     mtdev-1.1.6
+#
+
+export PKG="libinput-1.23.0"
+export PKGLOG_DIR=$LFSLOG/24.24.1.3
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+#export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+mkdir build
+cd    build
+
+echo "2. Meson Configure ..."
+echo "2. Meson Configure ..." >> $LFSLOG_PROCESS
+echo "2. Meson Configure ..." >> $PKGLOG_ERROR
+meson setup --prefix=$XORG_PREFIX       \
+            --buildtype=release         \
+            -Ddebug-gui=false           \
+            -Dtests=false               \
+            -Dlibwacom=false            \
+            -Dudev-dir=/usr/lib/udev    \
+            ..                          \
+        > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Ninja Build ..."
+echo "3. Ninja Build ..." >> $LFSLOG_PROCESS
+echo "3. Ninja Build ..." >> $PKGLOG_ERROR
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Ninja Test NOT executed, very compliated ..."
+echo "4. Ninja Test NOT executed, very compliated ..." >> $LFSLOG_PROCESS
+echo "4. Ninja Test NOT executed, very compliated ..." >> $PKGLOG_ERROR
+
+# echo "4. Ninja Test ..."
+# echo "4. Ninja Test ..." >> $LFSLOG_PROCESS
+# echo "4. Ninja Test ..." >> $PKGLOG_ERROR
+# ninja test > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Ninja Install ..."
+echo "5. Ninja Install ..." >> $LFSLOG_PROCESS
+echo "5. Ninja Install ..." >> $PKGLOG_ERROR
+ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+
+cd ..
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+#unset PKGLOG_OTHERS
+#unset PKGLOG_CHECK
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
