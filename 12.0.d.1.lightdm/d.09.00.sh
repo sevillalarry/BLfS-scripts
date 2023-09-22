cde40f28ff95ee3540cc8706602e26097fa372d5
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-09-22 15:34:17 +0800

    bug fix

--- 12.0.d.1.lightdm/d.09.00.sh
+++ 12.0.d.1.lightdm/d.09.00.sh
@@ -15,7 +15,7 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 echo "09.37.libgpg-error-1.47"
 echo "09.37.libgpg-error-1.47" >> $LFSLOG_PROCESS
 
-time { d.09.37.libgpg-error-1.47.sh ; }
+time { bash d.09.37.libgpg-error-1.47.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
@@ -29,7 +29,7 @@ echo "."
 echo "09.36.libgcrypt-1.10.2"
 echo "09.36.libgcrypt-1.10.2" >> $LFSLOG_PROCESS
 
-time { d.09.36.libgcrypt-1.10.2.sh ; }
+time { bash d.09.36.libgcrypt-1.10.2.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
