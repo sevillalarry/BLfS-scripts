24de7a2eb406f0468217378c04400b71aa687d69
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2023-11-27 21:09:51 +0800

    bug fix

--- 12.0.c.1.LXDE/c.03.90.all.support.apps.c.1.LXDE.sh
+++ 12.0.c.1.LXDE/c.03.90.all.support.apps.c.1.LXDE.sh
@@ -3,12 +3,12 @@
 # Support Applications
 #
 
+bash c.10.00-0.cDE.sh
+bash c.25.00-1.LXDE.Part1.sh
 bash c.09.00-1.LXDE.sh
 bash c.13.00-0.cDE.sh
 bash c.13.00-1.LXDE.Part1.sh
-bash c.10.00-0.cDE.sh
 bash c.33.00-0.cDE.sh
-bash c.25.00-1.LXDE.Part1.sh
 
 # Python2 Modules
 bash c.13.00-1.LXDE.Part2.sh
